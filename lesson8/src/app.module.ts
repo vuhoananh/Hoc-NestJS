ac71e992ad81701f7da6ac29dc8bdabbea22b05d
Vu Hoang Anh <hoanganh81100@gmail.com>
2023-02-07 22:17:12 +0700

    Learn NestJS

---
+++ lesson8/src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PostModule } from './post/post.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
+import { APP_FILTER } from '@nestjs/core';
+import { ExceptionLoggerFilter } from './utils/exceptionLogger.filter';
+
+@Module({
+  imports: [
+    PostModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    }),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: ExceptionLoggerFilter,
+    },
+  ],
+})
+export class AppModule {}
---
+++ lesson8/src/post/controllers/post.controller.ts
@@ -0,0 +1,47 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  UseFilters,
+} from '@nestjs/common';
+import { CreatePostDto, UpdatePostDto } from '../dto/post.dto';
+import { PostService } from '../services/post.service';
+import { ExceptionLoggerFilter } from '../../utils/exceptionLogger.filter';
+import { HttpExceptionFilter } from '../../utils/httpException.filter';
+
+@Controller('post')
+export class PostController {
+  constructor(private readonly postService: PostService) {}
+
+  @Get()
+  getAllPost() {
+    return this.postService.getAllPosts();
+  }
+
+  @Get(':id')
+  @UseFilters(HttpExceptionFilter)
+  // @UseFilters(ExceptionLoggerFilter)
+  getPostById(@Param('id') id: string) {
+    return this.postService.getPostById(id);
+  }
+
+  @Post()
+  async createPost(@Body() post: CreatePostDto) {
+    return this.postService.createPost(post);
+  }
+
+  @Put(':id')
+  async replacePost(@Param('id') id: string, @Body() post: UpdatePostDto) {
+    return this.postService.replacePost(id, post);
+  }
+
+  @Delete(':id')
+  async deletePost(@Param('id') id: string) {
+    await this.postService.deletePost(id);
+    return true;
+  }
+}
